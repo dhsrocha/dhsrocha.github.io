a22ad35c1ac1027f5c28796196a91ca7870b48e3
Diego Henrique da Silva Rocha <dhsrocha@gmail.com>
2020-07-21 20:32:35 +0100

    Added default serviceWorker.

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,11 @@
 "use strict";
-(function (feather) {
+(function (document, navigator, feather) {
   "use strict";
+  // https://www.pwabuilder.com/serviceworker
+  if ("serviceWorker" in navigator) {
+    navigator.serviceWorker.register("./scripts/sw.js");
+  }
   // https://github.com/feathericons/feather#3-use-1
   feather.replace({ "stroke-width": 1.75 });
-})(feather);
+  // :::
+})(document, navigator, feather);
