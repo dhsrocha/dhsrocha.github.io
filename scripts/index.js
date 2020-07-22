8cddeb9c11e71cc548e4f169d646edc1095cc4e9
Diego Henrique da Silva Rocha <dhsrocha@gmail.com>
2020-07-22 19:16:53 +0100

    Added QR Code self-references in pages;
    * Included QRious as dependency in pages and script;
    * Changed deeply in footer structure's style;

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,16 @@
 "use strict";
-(function (navigator) {
+(function (navigator, QRious) {
   "use strict";
   // https://www.pwabuilder.com/serviceworker
   if ("serviceWorker" in navigator) {
     navigator.serviceWorker.register("./scripts/sw.js");
   }
   // :::
-})(navigator);
+  const style = getComputedStyle(document.documentElement);
+  new QRious({
+    element: document.getElementById("qr"),
+    value: "https://dhsrocha.github.io",
+    foreground: style.getPropertyValue("--color-primary-tint-3"),
+    background: style.getPropertyValue("--color-secondary"),
+  });
+})(navigator, QRious);
