a15741f6ed4b4579e06816f9b348e6e3379f386a
Diego Henrique da Silva Rocha <dhsrocha@gmail.com>
2020-07-22 13:18:37 +0100

    Huge icons improvement using font-awesome.
    * Improved colors' tinting;
    * Replaced icons in HTML;

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,9 @@
 "use strict";
-(function (document, navigator, feather) {
+(function (navigator) {
   "use strict";
   // https://www.pwabuilder.com/serviceworker
   if ("serviceWorker" in navigator) {
     navigator.serviceWorker.register("./scripts/sw.js");
   }
-  // https://github.com/feathericons/feather#3-use-1
-  feather.replace({ "stroke-width": 1.75 });
   // :::
-})(document, navigator, feather);
+})(navigator);
