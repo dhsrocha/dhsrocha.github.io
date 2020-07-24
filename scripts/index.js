56b56910b6445810111e95afa46cf12837538b93
Diego Henrique da Silva Rocha <dhsrocha@gmail.com>
2020-07-24 22:58:56 +0100

    Little refactoring on script.

--- scripts/index.js
+++ scripts/index.js
@@ -2,9 +2,8 @@
 (function (window, document, navigator, QRious) {
   "use strict";
   // https://www.pwabuilder.com/serviceworker
-  if ("serviceWorker" in navigator) {
+  "serviceWorker" in navigator &&
     navigator.serviceWorker.register("./scripts/sw.js");
-  }
   // :::
   const style = getComputedStyle(document.documentElement);
   new QRious({
