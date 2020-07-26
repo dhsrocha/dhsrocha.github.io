422e2e8835d700c02dbf1f4269108fae3bc09d7f
Diego Henrique da Silva Rocha <dhsrocha@gmail.com>
2020-07-26 02:11:42 +0100

    Added 'Back to article top' button.

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,5 @@
 "use strict";
-(async function (window, document, navigator, QRious) {
+(async function (window, document, navigator, location, QRious) {
   "use strict";
   // ::: Constants
   const backToTopLabel = "Back to top";
@@ -26,21 +26,22 @@
     "state=closed&labels=blog-post&assignee=dhsrocha&page=1&per_page=1";
   const res = await fetch(url);
   const data = await res.json();
+
   // ::: Articles
   const articles = document.createElement("section");
+  articles.id = "articles";
   notes.appendChild(articles);
   data.forEach((post) => {
-    const h3 = document.createElement("h3");
-    h3.innerHTML = post.title;
+    const id = "article-" + post.id;
 
     const art = document.createElement("article");
-    art.appendChild(h3);
-
+    const h3 = document.createElement("h3");
     const p = document.createElement("p");
-    p.innerHTML = post.body;
-    art.appendChild(p);
 
-    articles.appendChild(art);
+    art.id = id;
+    h3.innerHTML = post.title;
+    p.innerHTML = post.body;
+
     // ::: Comment box
     const script = document.createElement("script");
     script.src = "https://utteranc.es/client.js";
@@ -50,7 +51,24 @@
     script.setAttribute("theme", "preferred-color-scheme");
     script.setAttribute("crossorigin", "anonymous");
     script.setAttribute("async", "async");
+
+    // "Back to post beginning" button
+    const div = document.createElement("div");
+    div.style =
+      "display: flex; justify-content: flex-end; cursor: pointer; padding: 1em 2em";
+
+    const em = document.createElement("em");
+    em.setAttribute("class", "fas fa-2x fa-sort-up");
+    em.setAttribute("title", "Back to article top");
+    em.onclick = () => (location.href = "#" + id);
+
+    articles.appendChild(art);
+    art.appendChild(h3);
+    art.appendChild(p);
     art.appendChild(script);
+    art.appendChild(div);
+    div.appendChild(em);
+    // :::
   });
 
   // ::: "Back to top" button
@@ -60,15 +78,16 @@
     const h3 = document.createElement("h3");
     const p = document.createElement("p");
 
+    h3.innerHTML = "I would like to hear you";
     p.innerHTML =
       "Reach me if you have an idea that you want to get it started. " +
       "I would be pleased to help you with something.";
-    h3.innerHTML = "I would like to hear you";
 
     msg.appendChild(h3);
     msg.appendChild(p);
     sec.appendChild(msg);
 
+    // "Back to top" button
     const btn = document.createElement("button");
     btn.onclick = () => window.scrollTo(0, 0);
     btn.setAttribute("class", "clear");
@@ -95,4 +114,4 @@
     // if (height < screen.height) toTop.style = "display: none";
     // sec.style = "";
   });
-})(window, document, navigator, QRious);
+})(window, document, navigator, location, QRious);
