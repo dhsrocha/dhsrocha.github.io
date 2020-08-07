65628647f34ad34d203badd8732aacceeb2d63cb
Diego Henrique da Silva Rocha <dhsrocha.dev@gmail.com>
2020-08-07 22:57:13 +0100

    Refactored service worker script..

--- scripts/sw.js
+++ scripts/sw.js
@@ -5,14 +5,10 @@ importScripts(
 );
 
 const CACHE = "pwabuilder-page";
-
-// TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
-const offlineFallbackPage = "../index.html";
+const offlineFallbackPage = "/index.html";
 
 self.addEventListener("message", (event) => {
-  if (event.data && event.data.type === "SKIP_WAITING") {
-    self.skipWaiting();
-  }
+  event.data && event.data.type === "SKIP_WAITING" && self.skipWaiting();
 });
 
 self.addEventListener("install", async (event) => {
@@ -21,9 +17,7 @@ self.addEventListener("install", async (event) => {
   );
 });
 
-if (workbox.navigationPreload.isSupported()) {
-  workbox.navigationPreload.enable();
-}
+if (workbox.navigationPreload.isSupported()) workbox.navigationPreload.enable();
 
 self.addEventListener("fetch", (event) => {
   if (event.request.mode === "navigate") {
@@ -31,11 +25,7 @@ self.addEventListener("fetch", (event) => {
       (async () => {
         try {
           const preloadResp = await event.preloadResponse;
-
-          if (preloadResp) {
-            return preloadResp;
-          }
-
+          if (preloadResp) return preloadResp;
           const networkResp = await fetch(event.request);
           return networkResp;
         } catch (error) {
