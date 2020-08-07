0085c83bfd1f84f3f8a4693eaf012858450e3e86
Diego Henrique da Silva Rocha <dhsrocha.dev@gmail.com>
2020-08-08 00:12:22 +0100

    Updated imports in sw.js.
    * Wrapped into a IIFE boilerplate;
    * Updated workbox CDN's version;
    * Implemented some code snippet from workbox.

--- sw.js
+++ sw.js
@@ -1,39 +1,54 @@
-// This is the "Offline page" service worker
-
-importScripts(
-  "https://storage.googleapis.com/workbox-cdn/releases/5.0.0/workbox-sw.js"
-);
-
-const CACHE = "pwabuilder-page";
-const offlineFallbackPage = "/index.html";
-
-self.addEventListener("message", (event) => {
-  event.data && event.data.type === "SKIP_WAITING" && self.skipWaiting();
-});
-
-self.addEventListener("install", async (event) => {
-  event.waitUntil(
-    caches.open(CACHE).then((cache) => cache.add(offlineFallbackPage))
+"use strict";
+(function () {
+  "use strict";
+  importScripts(
+    "https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js"
   );
-});
 
-if (workbox.navigationPreload.isSupported()) workbox.navigationPreload.enable();
+  const CACHE = "cached-page";
+  const offlineFallbackPage = "/index.html";
 
-self.addEventListener("fetch", (event) => {
-  if (event.request.mode === "navigate") {
-    event.respondWith(
-      (async () => {
-        try {
-          const preloadResp = await event.preloadResponse;
-          if (preloadResp) return preloadResp;
-          const networkResp = await fetch(event.request);
-          return networkResp;
-        } catch (error) {
-          const cache = await caches.open(CACHE);
-          const cachedResp = await cache.match(offlineFallbackPage);
-          return cachedResp;
-        }
-      })()
+  // https://developers.google.com/web/tools/workbox/modules/workbox-sw
+  workbox.loadModule("workbox-strategies");
+
+  self.addEventListener("message", (event) => {
+    // https://www.pwabuilder.com/serviceworker
+    event.data && event.data.type === "SKIP_WAITING" && self.skipWaiting();
+  });
+
+  self.addEventListener("install", async (event) => {
+    // https://www.pwabuilder.com/serviceworker
+    event.waitUntil(
+      caches.open(CACHE).then((cache) => cache.add(offlineFallbackPage))
     );
-  }
-});
+  });
+
+  if (workbox.navigationPreload.isSupported())
+    workbox.navigationPreload.enable();
+
+  self.addEventListener("fetch", (event) => {
+    // https://www.pwabuilder.com/serviceworker
+    if (event.request.mode === "navigate") {
+      event.respondWith(
+        (async () => {
+          try {
+            const preloadResp = await event.preloadResponse;
+            if (preloadResp) return preloadResp;
+            const networkResp = await fetch(event.request);
+            return networkResp;
+          } catch (error) {
+            const cache = await caches.open(CACHE);
+            const cachedResp = await cache.match(offlineFallbackPage);
+            return cachedResp;
+          }
+        })()
+      );
+    }
+
+    // https://developers.google.com/web/tools/workbox/modules/workbox-sw
+    if (event.request.url.endsWith(".png")) {
+      const cacheFirst = new strategies.CacheFirst();
+      event.respondWith(cacheFirst.handle({ request: event.request }));
+    }
+  });
+})();
